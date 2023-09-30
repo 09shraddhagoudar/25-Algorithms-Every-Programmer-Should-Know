ed5a6dda77a22a3a39eb472bc80eeba1483082a5
shraddha krishnagoudar <146472639+09shraddhagoudar@users.noreply.github.com>
2023-10-01 00:15:55 +0530

    Update LinearSearch.java

--- 1 . Searching/LinearSearch.java
+++ 1 . Searching/LinearSearch.java
@@ -38,3 +38,25 @@ public class LinearSearch {
         sc.close();
     }
 }
+/*This Java program implements the linear search algorithm to find the index of a target element in an array. Here's a concise description of its key components and functionality:
+
+linearSearch method:
+
+Performs linear search by iterating through the array from start to end.
+Compares each element with the target element.
+If the element is found, it returns its index.
+If the entire array is searched without finding the target element, it returns -1 to indicate that the element was not found.
+main method:
+
+Accepts user input for the length of the array, the elements of the array, and the target element to search for.
+Creates an integer array based on user input.
+Calls the linearSearch method to find the index of the target element.
+If the element is found (index != -1), it prints the index where the element was found.
+If the element is not found, it prints a message indicating that the element was not found in the array.
+This program is designed to search for a target element in an array using a simple linear search, making it suitable for small arrays or when the array is not sorted.*/
+
+
+
+
+
+

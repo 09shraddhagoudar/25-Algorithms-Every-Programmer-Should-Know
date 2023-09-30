35be5b5b1fab6930e6af457c14e1d0c06af4cbdb
shraddha krishnagoudar <146472639+09shraddhagoudar@users.noreply.github.com>
2023-10-01 00:10:59 +0530

    Update BinarySearch.java

--- 1 . Searching/BinarySearch.java
+++ 1 . Searching/BinarySearch.java
@@ -52,3 +52,28 @@ public class BinarySearch {
         sc.close();
     }
 }
+/*This Java program implements the binary search algorithm to find the index of a target element in a sorted array. Here's a brief description of its key components and functionality:
+
+binarySearch method:
+
+Performs binary search on a sorted integer array to find the index of a target element.
+Maintains two pointers, left and right, initially set to the start and end of the array, respectively.
+Iteratively calculates the middle index (mid) and compares the middle element to the target.
+If the middle element equals the target, it returns the index of the element.
+If the middle element is less than the target, it updates left to mid + 1 to search in the right half.
+If the middle element is greater than the target, it updates right to mid - 1 to search in the left half.
+Continues the process until the target is found or the search range is exhausted.
+main method:
+
+Accepts user input for the length of a sorted array, the sorted elements of the array, and the target element to search for.
+Creates an integer array based on user input and sorts it using Arrays.sort.
+Calls the binarySearch method to find the index of the target element.
+If the element is found (index != -1), it prints the index where the element was found.
+If the element is not found, it prints a message indicating that the element was not found in the array.
+This program is designed to search for a target element efficiently in a sorted array using binary search, making it suitable for large arrays where traditional linear search would be less efficient.*/
+
+
+
+
+
+
